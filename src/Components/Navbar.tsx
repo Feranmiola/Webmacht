e3e49c8828ba5d4cc9996ee257204f07b5462ce2
Feranmiola <88761161+Feranmiola@users.noreply.github.com>
2024-11-04 19:52:36 +0100

    "Updated Navbar component: changed font size of menu items from 'text-sm' to 'text-base', added 'Get In Touch' button"

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -8,10 +8,14 @@ const Navbar = () => {
                 <div className="flex flex-row space-x-[5rem] items-center">
                     <Webmacht />
                     <div className="flex flex-row items-center space-x-10">
-                        <p className="text-sm text-white opacity-70 hover:opacity-100 font-medium hover:underline underline-offset-4 decoration-deep-purple cursor-pointer transition ease-in-out">About</p>
-                        <p className="text-sm text-white opacity-70 hover:opacity-100 font-medium hover:underline underline-offset-4 decoration-deep-purple cursor-pointer transition ease-in-out">Services</p>
-                        <p className="text-sm text-white opacity-70 hover:opacity-100 font-medium hover:underline underline-offset-4 decoration-deep-purple cursor-pointer transition ease-in-out">Industries We Serve</p>
+                        <p className="text-base text-white opacity-70 hover:opacity-100 font-medium hover:underline underline-offset-4 decoration-deep-purple cursor-pointer transition ease-in-out">About</p>
+                        <p className="text-base text-white opacity-70 hover:opacity-100 font-medium hover:underline underline-offset-4 decoration-deep-purple cursor-pointer transition ease-in-out">Services</p>
+                        <p className="text-base text-white opacity-70 hover:opacity-100 font-medium hover:underline underline-offset-4 decoration-deep-purple cursor-pointer transition ease-in-out">Industries We Serve</p>
                     </div>
+                </div>
+
+                <div className='flex items-center justify-center px-2 py-1 cursor-pointer rounded-[4px] text-base font-medium text-white border-[1px] border-white hover:border-deep-purple hover:text-deep-purple transition ease-in-out'>
+                    Get In Touch
                 </div>
             </div>
 
