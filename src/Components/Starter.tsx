7a043756c8a9ea14d42287a9bdb5ae4b617ce0f5
Feranmiola <88761161+Feranmiola@users.noreply.github.com>
2024-11-04 20:41:16 +0100

    "Added hero section content to Starter component"

--- src/Components/Starter.tsx
+++ src/Components/Starter.tsx
@@ -3,6 +3,15 @@ import React from 'react'
 const Starter = () => {
     return (
         <div className='homeBackground h-[911px] w-full'>
+            <div className='flex flex-col h-full justify-end pl-20 pb-[8rem]'>
+                <div className='flex flex-col space-y-8'>
+                    <p className='text-white text-[72px] font-semibold leading-[5rem] w-[772px]'>Empowering Digital Transformation for Businesses</p>
+                    <p className='text-white w-[752px] text-base font-light'>Your trusted partner to deliver cutting-edge web development solutions tailored to your business needs. Crafting innovative digital experiences for businesses in Germany and across the globe.</p>
+                    <div className='bg-deep-purple text-white text-base font-bold border-[1px] border-transparent rounded-[4px] flex items-center justify-center h-[48px] w-[162px] hover:border-white hover:bg-transparent cursor-pointer transition ease-in-out '>
+                        Get In Touch
+                    </div>
+                </div>
+            </div>
 
         </div>
     )
