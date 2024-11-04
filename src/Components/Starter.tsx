71c3a6e93a56d9f8d45dbf396d38cf0051ce2c89
Feranmiola <88761161+Feranmiola@users.noreply.github.com>
2024-11-04 20:08:41 +0100

    "Added scrollbar hiding styles to globals.css and imported Starter component in page.tsx"

---
+++ src/Components/Starter.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Starter = () => {
+    return (
+        <div className='homeBackground h-[911px] w-full'>
+
+        </div>
+    )
+}
+
+export default Starter
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import Webmacht from "@/Components/Icons/Webmacht";
 import Navbar from "@/Components/Navbar";
+import Starter from "@/Components/Starter";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div className="w-full h-full flex">
       <Navbar />
+      <Starter />
 
     </div>
   );
