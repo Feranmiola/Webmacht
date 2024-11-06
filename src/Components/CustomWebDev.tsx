5bf0469fc0729e3a0b405f24798f0230411ea395
Feranmiola <88761161+Feranmiola@users.noreply.github.com>
2024-11-06 16:16:53 +0100

    "Added WebmachtLogoSquare component and modified CustomWebDev component layout and styling"

--- src/Components/CustomWebDev.tsx
+++ src/Components/CustomWebDev.tsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import WebmachtLogoSquare from './Icons/WebmachtLogoSquare'
 
 const CustomWebDev = () => {
     return (
-        <div className='bg-black w-full h-[1006px]'>
+        <div className='bg-black w-full h-[1006px] flex items-center flex-col space-y-10 justify-center'>
+            <div className='flex flex-row w-[1290px] h-[134px] items-center justify-between'>
+                <p className='w-[654px] text-white text-[56px] font-semibold'>Comprehensive Web Development Solutions</ p>
+                <div className='flex flex-col w-[530px] h-full justify-between'>
+                    <WebmachtLogoSquare />
+                    <p className='text-white text-base w-full'>From custom website design to full-stack development, we help businesses thrive in the digital world.</p>
+                </div>
+            </div>
 
         </div>
     )
---
+++ src/Components/Icons/WebmachtLogoSquare.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const WebmachtLogoSquare = () => {
+    return (
+        <svg width="40" height="37" viewBox="0 0 40 37" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M16.1498 24.6426V26.5992H8.02539V24.6426H16.1498Z" fill="white" />
+            <path d="M20.0066 24.6427L16.4619 12.2646H19.326L21.3678 20.857H21.467L23.7214 12.2646H26.1743L28.4288 20.8853H28.5422L30.5839 12.2646H33.448L29.9034 24.6427H27.3512L24.9975 16.5466H24.8983L22.5446 24.6427H19.9924H20.0066Z" fill="white" />
+            <path d="M37.1642 2.83575V33.476H2.83575V2.83575H37.1642ZM40 0H0V36.3117H40V0Z" fill="white" />
+        </svg>
+
+    )
+}
+
+export default WebmachtLogoSquare
