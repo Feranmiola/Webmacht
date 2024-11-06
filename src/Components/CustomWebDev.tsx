5ce7499f9ce626aa954cfd17973b7e82fab5761c
Feranmiola <88761161+Feranmiola@users.noreply.github.com>
2024-11-06 16:55:25 +0100

    "Added new components and layout to CustomWebDev component"

--- src/Components/CustomWebDev.tsx
+++ src/Components/CustomWebDev.tsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import WebmachtLogoSquare from './Icons/WebmachtLogoSquare'
+import CustomDevIcon from './Icons/CustomDevIcon'
+import BlueLine from './Icons/BlueLine'
+import BlueDot from './Icons/BlueDot'
 
 const CustomWebDev = () => {
     return (
@@ -10,6 +13,36 @@ const CustomWebDev = () => {
                     <WebmachtLogoSquare />
                     <p className='text-white text-base w-full'>From custom website design to full-stack development, we help businesses thrive in the digital world.</p>
                 </div>
+            </div>
+
+            <div className='w-[1289px] h-[560px] flex relative items-start justify-start'>
+                <div className='h-full w-[519px] rounded-[24px] border-[1.5px] border-[#1B1B1B] bg-[#080808] flex flex-col justify-evenly items-center'>
+                    <div className='h-[275px] w-[443px] flex flex-col justify-between'>
+                        <CustomDevIcon />
+                        <p className='text-white font-light leading-[4rem] text-[48px] w-full'>Custom Web Development</p>
+                        <p className='text-sm text-white w-full'>Tailor-made websites, custom web apps, and platforms that meet the specific needs of your business, whether you’re a startup or a large enterprise.</p>
+                    </div>
+                    <BlueLine />
+
+                    <div className='h-[130px] w-[443px] flex flex-col justify-between'>
+                        <div className='flex flex-row space-x-2 items-center'>
+                            <BlueDot />
+                            <p className='text-white text-base'>Frontend Development</p>
+                        </div>
+
+                        <div className='flex flex-row space-x-2 items-center'>
+                            <BlueDot />
+                            <p className='text-white text-base'>Backend Development</p>
+                        </div>
+
+                        <div className='flex flex-row space-x-2 items-center'>
+                            <BlueDot />
+                            <p className='text-white text-base'>CMS Development</p>
+                        </div>
+
+                    </div>
+                </div>
+
             </div>
 
         </div>
