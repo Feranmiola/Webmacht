da5ecef8cf2b4054d16397d7b67663c81441dad6
Feranmiola <88761161+Feranmiola@users.noreply.github.com>
2024-11-07 23:46:57 +0100

    "Added custom scrollbar styles and functionality to globals.css, and updated page.tsx to handle scrollbar updates on scroll and resize events."

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+'use client'
 import Navbar from "@/Components/Navbar";
 import Partner from "@/Components/Partner";
 import Starter from "@/Components/Starter";
@@ -10,7 +11,34 @@ import Contact from "@/Components/Contact";
 import Footer from "@/Components/Footer";
 import CustomWebDev from "@/Components/CustomWebDev";
 
+function updateScrollbar() {
+  let scrollTimer;
+
+  const scrollPercentage = window.scrollY / (document.documentElement.scrollHeight - window.innerHeight);
+  const scrollbarHeight = (window.innerHeight / document.documentElement.scrollHeight) * window.innerHeight;
+  const scrollTop = scrollPercentage * (window.innerHeight - scrollbarHeight);
+
+  document.body.style.setProperty('--scroll-top', `${scrollTop}px`);
+  document.body.style.setProperty('--scrollbar-height', `${scrollbarHeight}px`);
+
+  document.body.classList.add('is-scrolling');
+  //@ts-ignore
+  clearTimeout(scrollTimer);
+  scrollTimer = setTimeout(() => {
+    document.body.classList.remove('is-scrolling');
+  }, 1000);
+}
+
+window.addEventListener('scroll', updateScrollbar);
+window.addEventListener('resize', updateScrollbar);
+
+// Initial call to set the correct size
+updateScrollbar();
+
+
+
 export default function Home() {
+
   return (
     <div className={`w-full h-full flex flex-col ${GeistSans.className}`}>
       <Navbar />
