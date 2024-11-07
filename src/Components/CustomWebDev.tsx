da021d7ab9a84fb20d42557dd252392158a35c0f
Feranmiola <88761161+Feranmiola@users.noreply.github.com>
2024-11-07 20:08:39 +0100

    "Updated spacing in CustomWebDev component from space-y-10 to space-y-20"

--- src/Components/CustomWebDev.tsx
+++ src/Components/CustomWebDev.tsx
@@ -43,7 +43,7 @@ const CustomWebDev = () => {
     }
 
     return (
-        <div className='bg-black w-full h-[1006px] flex items-center flex-col space-y-10 justify-center'>
+        <div className='bg-black w-full h-[1006px] flex items-center flex-col space-y-20 justify-center'>
             <div className='flex flex-row w-[1290px] h-[134px] items-center justify-between'>
                 <p className='w-[654px] text-white text-[56px] font-semibold'>Comprehensive Web Development Solutions</p>
                 <div className='flex flex-col w-[530px] h-full justify-between'>
