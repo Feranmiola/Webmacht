c84b2220efc69a3c0fbf1223f0bc90ae5fe220aa
Feranmiola <88761161+Feranmiola@users.noreply.github.com>
2024-11-07 14:28:08 +0100

    "Refactored CustomWebDev component: updated icon imports, removed hover state, and reorganized tab content structure."

--- src/Components/CustomWebDev.tsx
+++ src/Components/CustomWebDev.tsx
@@ -12,9 +12,10 @@ import MaintainanceIcon from './Icons/MaintainanceIcon'
 import ForwardIcon from './Icons/ForwardIcon'
 import CustomDevIconSmall from './Icons/CustomDevIconSmall'
 import DesignIconBig from './Icons/DesignIconBig'
+import EcommerceIconBig from './Icons/EcommerceIconBig'
 
 const CustomWebDev = () => {
-    const [hoverIndec, setHoverIndex] = useState(0)
+    // const [hoverIndec, setHoverIndex] = useState(0)
     const [selectedTab, setSelectedTab] = useState(2)
 
 
@@ -131,7 +132,7 @@ const CustomWebDev = () => {
                     <div className={`w-full h-full flex flex-col py-10 border-l-[1.5px] border-l-[#1B1B1B] border-t-[1.5px] border-t-[#1B1B1B]  rounded-[24px] ${selectedTab === 3 ? 'bg-custom-gradient justify-between px-10 ' : 'gradient-container space-y-10 items-center'}`}>
 
                         {selectedTab === 3 ? (
-                            <DesignIconBig />
+                            <EcommerceIconBig />
                         ) : (
                             <EcommerceIcon />
                         )}
@@ -171,133 +172,144 @@ const CustomWebDev = () => {
                 </div>
 
 
-
-
                 <div
                     onClick={() => setSelectedTab(4)}
                     className={`${selectedTab === 4 ? 'h-full w-[519px]' : 'w-[168px] h-[560px]'}`}>
-                    {selectedTab === 4 ? (
-                        <div className='h-full w-[519px] rounded-[24px] bg-custom-gradient border-l-[1.5px] border-l-[#1B1B1B] border-t-[1.5px] border-t-[#1B1B1B]  flex flex-col justify-evenly px-10 items-center'>
-                            <div className='h-[275px] w-[443px] flex flex-col justify-between'>
-                                <MobileIcon />
-                                <p className='text-white font-light leading-[4rem] text-[48px] w-full'>Mobile App Development</p>
-                                <p className='text-sm text-white w-full'>Tailor-made websites, custom web apps, and platforms that meet the specific needs of your business, whether you're a startup or a large enterprise.</p>
-                            </div>
-                            <BlueLine />
+                    <div className={`w-full h-full flex flex-col py-10 border-l-[1.5px] border-l-[#1B1B1B] border-t-[1.5px] border-t-[#1B1B1B]  rounded-[24px] ${selectedTab === 4 ? 'bg-custom-gradient justify-between px-10 ' : 'gradient-container space-y-10 items-center'}`}>
 
-                            <div className='h-[130px] w-[443px] flex flex-col justify-between'>
-                                <div className='flex flex-row space-x-2 items-center'>
-                                    <BlueDot />
-                                    <p className='text-white text-base'>Frontend Development</p>
-                                </div>
-
-                                <div className='flex flex-row space-x-2 items-center'>
-                                    <BlueDot />
-                                    <p className='text-white text-base'>Backend Development</p>
-                                </div>
-
-                                <div className='flex flex-row space-x-2 items-center'>
-                                    <BlueDot />
-                                    <p className='text-white text-base'>CMS Development</p>
-                                </div>
-                            </div>
-                        </div>
-                    ) : (
-                        <div
-                            onMouseEnter={() => setHoverIndex(4)}
-                            onMouseLeave={() => setHoverIndex(0)}
-                            className='w-[168px] h-[560px]  gradient-container flex flex-col items-center pt-10 space-y-10 transition ease-in-out rounded-[24px] border-l-[1.5px] border-l-[#1B1B1B] border-t-[1.5px] border-t-[#1B1B1B] bg-[#080808]'>
+                        {selectedTab === 4 ? (
+                            <EcommerceIconBig />
+                        ) : (
                             <MobileIcon />
-                            <p className='text-white text-[32px] opacity-70 whitespace-nowrap pl-20 rotate-90 w-full'>Mobile App Development</p>
-                        </div>
-                    )}
+                        )}
+                        {selectedTab === 4 ? (
+                            <>
+                                <div className='w-[443px] flex flex-col justify-between space-y-3'>
+                                    <p className='text-white font-light leading-[4rem] text-[48px] w-full'>Mobile App Development</p>
+                                    <p className='text-sm text-white w-full'>Tailor-made websites, custom web apps, and platforms that meet the specific needs of your business, whether you're a startup or a large enterprise.</p>
+                                </div>
+                                <div className='flex flex-col space-y-10'>
+                                    <BlueLine />
 
+                                    <div className='h-[130px] w-[443px] flex flex-col justify-between'>
+                                        <div className='flex flex-row space-x-2 items-center'>
+                                            <BlueDot />
+                                            <p className='text-white text-base'>Frontend Development</p>
+                                        </div>
+
+                                        <div className='flex flex-row space-x-2 items-center'>
+                                            <BlueDot />
+                                            <p className='text-white text-base'>Backend Development</p>
+                                        </div>
+
+                                        <div className='flex flex-row space-x-2 items-center'>
+                                            <BlueDot />
+                                            <p className='text-white text-base'>CMS Development</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </>
+                        ) : (
+                            <>
+                                <p className='text-white text-[32px] opacity-70 whitespace-nowrap pl-20 rotate-90 w-full'>Mobile App Development</p>
+                            </>
+                        )}
+                    </div>
                 </div>
+
+
 
 
                 <div
                     onClick={() => setSelectedTab(5)}
                     className={`${selectedTab === 5 ? 'h-full w-[519px]' : 'w-[168px] h-[560px]'}`}>
-                    {selectedTab === 5 ? (
-                        <div className='h-full w-[519px] rounded-[24px] bg-custom-gradient border-l-[1.5px] border-l-[#1B1B1B] border-t-[1.5px] border-t-[#1B1B1B]  flex flex-col justify-evenly px-10 items-center'>
-                            <div className='h-[275px] w-[443px] flex flex-col justify-between'>
-                                <ApiIcon />
-                                <p className='text-white font-light leading-[4rem] text-[48px] w-full'>API Integrations</p>
-                                <p className='text-sm text-white w-full'>Tailor-made websites, custom web apps, and platforms that meet the specific needs of your business, whether you're a startup or a large enterprise.</p>
-                            </div>
-                            <BlueLine />
+                    <div className={`w-full h-full flex flex-col py-10 border-l-[1.5px] border-l-[#1B1B1B] border-t-[1.5px] border-t-[#1B1B1B]  rounded-[24px] ${selectedTab === 5 ? 'bg-custom-gradient justify-between px-10 ' : 'gradient-container space-y-10 items-center'}`}>
 
-                            <div className='h-[130px] w-[443px] flex flex-col justify-between'>
-                                <div className='flex flex-row space-x-2 items-center'>
-                                    <BlueDot />
-                                    <p className='text-white text-base'>Frontend Development</p>
-                                </div>
-
-                                <div className='flex flex-row space-x-2 items-center'>
-                                    <BlueDot />
-                                    <p className='text-white text-base'>Backend Development</p>
-                                </div>
-
-                                <div className='flex flex-row space-x-2 items-center'>
-                                    <BlueDot />
-                                    <p className='text-white text-base'>CMS Development</p>
-                                </div>
-                            </div>
-                        </div>
-                    ) : (
-                        <div
-                            onMouseEnter={() => setHoverIndex(5)}
-                            onMouseLeave={() => setHoverIndex(0)}
-                            className='w-[168px] h-[560px]  gradient-container flex flex-col items-center pt-10 space-y-10 transition ease-in-out rounded-[24px] border-l-[1.5px] border-l-[#1B1B1B] border-t-[1.5px] border-t-[#1B1B1B] bg-[#080808]'>
+                        {selectedTab === 5 ? (
+                            <EcommerceIconBig />
+                        ) : (
                             <ApiIcon />
-                            <p className='text-white text-[32px] opacity-70 whitespace-nowrap pl-20 rotate-90 w-full'>API Integrations</p>
-                        </div>
-                    )}
+                        )}
+                        {selectedTab === 5 ? (
+                            <>
+                                <div className='w-[443px] flex flex-col justify-between space-y-3'>
+                                    <p className='text-white font-light leading-[4rem] text-[48px] w-full'>API Integrations</p>
+                                    <p className='text-sm text-white w-full'>Tailor-made websites, custom web apps, and platforms that meet the specific needs of your business, whether you're a startup or a large enterprise.</p>
+                                </div>
+                                <div className='flex flex-col space-y-10'>
+                                    <BlueLine />
 
+                                    <div className='h-[130px] w-[443px] flex flex-col justify-between'>
+                                        <div className='flex flex-row space-x-2 items-center'>
+                                            <BlueDot />
+                                            <p className='text-white text-base'>Frontend Development</p>
+                                        </div>
+
+                                        <div className='flex flex-row space-x-2 items-center'>
+                                            <BlueDot />
+                                            <p className='text-white text-base'>Backend Development</p>
+                                        </div>
+
+                                        <div className='flex flex-row space-x-2 items-center'>
+                                            <BlueDot />
+                                            <p className='text-white text-base'>CMS Development</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </>
+                        ) : (
+                            <>
+                                <p className='text-white text-[32px] opacity-70 whitespace-nowrap pl-20 rotate-90 w-full'>API Integrations</p>
+                            </>
+                        )}
+                    </div>
                 </div>
-
-
 
                 <div
                     onClick={() => setSelectedTab(6)}
                     className={`${selectedTab === 6 ? 'h-full w-[519px]' : 'w-[168px] h-[560px]'}`}>
-                    {selectedTab === 6 ? (
-                        <div className='h-full w-[519px] rounded-[24px] bg-custom-gradient border-l-[1.5px] border-l-[#1B1B1B] border-t-[1.5px] border-t-[#1B1B1B]  flex flex-col justify-evenly px-10 items-center'>
-                            <div className='h-[275px] w-[443px] flex flex-col justify-between'>
-                                <MaintainanceIcon />
-                                <p className='text-white font-light leading-[4rem] text-[48px] w-full'>Maintenance & Support</p>
-                                <p className='text-sm text-white w-full'>Tailor-made websites, custom web apps, and platforms that meet the specific needs of your business, whether you're a startup or a large enterprise.</p>
-                            </div>
-                            <BlueLine />
+                    <div className={`w-full h-full flex flex-col py-10 border-l-[1.5px] border-l-[#1B1B1B] border-t-[1.5px] border-t-[#1B1B1B]  rounded-[24px] ${selectedTab === 6 ? 'bg-custom-gradient justify-between px-10 ' : 'gradient-container space-y-10 items-center'}`}>
 
-                            <div className='h-[130px] w-[443px] flex flex-col justify-between'>
-                                <div className='flex flex-row space-x-2 items-center'>
-                                    <BlueDot />
-                                    <p className='text-white text-base'>Frontend Development</p>
-                                </div>
-
-                                <div className='flex flex-row space-x-2 items-center'>
-                                    <BlueDot />
-                                    <p className='text-white text-base'>Backend Development</p>
-                                </div>
-
-                                <div className='flex flex-row space-x-2 items-center'>
-                                    <BlueDot />
-                                    <p className='text-white text-base'>CMS Development</p>
-                                </div>
-                            </div>
-                        </div>
-                    ) : (
-                        <div
-                            onMouseEnter={() => setHoverIndex(6)}
-                            onMouseLeave={() => setHoverIndex(0)}
-                            className='w-[168px] h-[560px]  gradient-container flex flex-col items-center pt-10 space-y-10 transition ease-in-out rounded-[24px] border-l-[1.5px] border-l-[#1B1B1B] border-t-[1.5px] border-t-[#1B1B1B] bg-[#080808]'>
+                        {selectedTab === 6 ? (
+                            <EcommerceIconBig />
+                        ) : (
                             <MaintainanceIcon />
-                            <p className='text-white text-[32px] opacity-70 whitespace-nowrap pl-20 rotate-90 w-full'>Maintenance & Support</p>
-                        </div>
-                    )}
+                        )}
+                        {selectedTab === 6 ? (
+                            <>
+                                <div className='w-[443px] flex flex-col justify-between space-y-3'>
+                                    <p className='text-white font-light leading-[4rem] text-[48px] w-full'>Maintenance & Support</p>
+                                    <p className='text-sm text-white w-full'>Tailor-made websites, custom web apps, and platforms that meet the specific needs of your business, whether you're a startup or a large enterprise.</p>
+                                </div>
+                                <div className='flex flex-col space-y-10'>
+                                    <BlueLine />
 
+                                    <div className='h-[130px] w-[443px] flex flex-col justify-between'>
+                                        <div className='flex flex-row space-x-2 items-center'>
+                                            <BlueDot />
+                                            <p className='text-white text-base'>Frontend Development</p>
+                                        </div>
+
+                                        <div className='flex flex-row space-x-2 items-center'>
+                                            <BlueDot />
+                                            <p className='text-white text-base'>Backend Development</p>
+                                        </div>
+
+                                        <div className='flex flex-row space-x-2 items-center'>
+                                            <BlueDot />
+                                            <p className='text-white text-base'>CMS Development</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </>
+                        ) : (
+                            <>
+                                <p className='text-white text-[32px] opacity-70 whitespace-nowrap pl-20 rotate-90 w-full'>Maintenance & Support</p>
+                            </>
+                        )}
+                    </div>
                 </div>
+
 
                 <div className='absolute top-[20.3rem] left-[28.2rem] z-50'>
                     <ForwardIcon />
