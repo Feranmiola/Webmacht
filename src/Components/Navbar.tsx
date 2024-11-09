68fe460b6b1c890f20aee5930b83ec0173a15bff
Feranmiola <88761161+Feranmiola@users.noreply.github.com>
2024-11-09 11:27:41 +0100

    "Commented out navigation links in Navbar component"

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -11,11 +11,11 @@ const Navbar = () => {
                     <div className=' cursor-pointer' onClick={() => router.push('/')}>
                         <Webmacht />
                     </div>
-                    <div className="flex flex-row items-center space-x-10">
+                    {/* <div className="flex flex-row items-center space-x-10">
                         <p className="text-base text-white opacity-70 hover:opacity-100 font-medium hover:underline underline-offset-4 decoration-deep-purple cursor-pointer transition ease-in-out">About</p>
                         <p className="text-base text-white opacity-70 hover:opacity-100 font-medium hover:underline underline-offset-4 decoration-deep-purple cursor-pointer transition ease-in-out">Services</p>
                         <p className="text-base text-white opacity-70 hover:opacity-100 font-medium hover:underline underline-offset-4 decoration-deep-purple cursor-pointer transition ease-in-out">Industries We Serve</p>
-                    </div>
+                    </div> */}
                 </div>
 
                 <div className='flex items-center justify-center px-2 py-1 cursor-pointer rounded-[4px] text-base font-medium text-white border-[1px] border-white hover:border-deep-purple hover:text-deep-purple transition ease-in-out'>
