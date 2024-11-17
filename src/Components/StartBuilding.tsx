4c051f35da9fd7bd52efe0c7307b95d6e9f49cdb
Feranmiola <88761161+Feranmiola@users.noreply.github.com>
2024-11-17 17:26:18 +0100

    "Translated text in StartBuilding component from English to German"

--- src/Components/StartBuilding.tsx
+++ src/Components/StartBuilding.tsx
@@ -3,9 +3,9 @@ import React from 'react'
 const StartBuilding = () => {
     return (
         <div className='bg-white my-10 h-[788px] w-full startBuilfingBG flex flex-col space-y-10 items-center justify-center'>
-            <p className='text-[80px] text-datkGrey font-semibold'><span className='text-deep-purple'>Start Building</span> with us.</p>
-            <div className='bg-deep-purple text-white hover:text-deep-purple text-base font-bold border-[1px] border-transparent rounded-[4px] flex items-center justify-center h-[48px] w-[162px] hover:border-deep-purple hover:bg-transparent cursor-pointer transition ease-in-out '>
-                Get In Touch
+            <p className='text-[80px] text-datkGrey font-semibold'><span className='text-deep-purple'>Beginnen Sie</span> mit uns zu bauen.</p>
+            <div className='bg-deep-purple text-white hover:text-deep-purple text-base font-bold border-[1px] border-transparent rounded-[4px] flex items-center justify-center h-[48px] px-4 hover:border-deep-purple hover:bg-transparent cursor-pointer transition ease-in-out '>
+                Kontakt aufnehmen
             </div>
         </div>
     )
