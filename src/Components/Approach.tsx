b6558794008f35377d4d7e21efd4de3754681a02
Feranmiola <88761161+Feranmiola@users.noreply.github.com>
2024-11-17 17:19:03 +0100

    "Translated English text to German in Approach component"

--- src/Components/Approach.tsx
+++ src/Components/Approach.tsx
@@ -6,25 +6,25 @@ import LaunchIcon from './Icons/LaunchIcon';
 const Approach = () => {
     return (
         <div className="relative bg-black pb-20">
-            {/* Container for sticky sections */}
+            {/* Container für Sticky-Bereiche */}
             <div className="h-[2631px] w-full py-10">
-                {/* Page 1 */}
+                {/* Seite 1 */}
                 <div className='sticky top-[110px] h-screen bg-black '>
                     <div id="page1" className="w-[1272px] h-[792px]  bg-[#090909] z-10 absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
                         <div className='flex items-center w-full h-full '>
                             <div className='flex-row flex space-x-[6rem]'>
                                 <img
                                     src="https://res.cloudinary.com/debiu7z1b/image/upload/v1730806886/Frame_44_fvvdqm.webp"
-                                    alt="Consultation & Discovery"
+                                    alt="Beratung & Entdeckung"
                                     className="h-[600px] w-[489px]"
                                 />
                                 <div className="flex flex-col h-[408.5px] justify-between">
-                                    <p className="text-white font-light z-[9999] text-[56px]">Our approach</p>
+                                    <p className="text-white font-light z-[9999] text-[56px]">Unser Ansatz</p>
                                     <div className="flex flex-col space-y-2">
                                         <ConsultationIcon />
                                         <div className="flex flex-col">
-                                            <p className="text-white font-bold text-[36px]">Consultation & Discovery</p>
-                                            <p className="text-white text-xl">Understanding your business goals and vision.</p>
+                                            <p className="text-white font-bold text-[36px]">Beratung & Entdeckung</p>
+                                            <p className="text-white text-xl">Verständnis Ihrer Geschäftsziele und Vision.</p>
                                         </div>
                                     </div>
                                 </div>
@@ -33,21 +33,21 @@ const Approach = () => {
                     </div>
                 </div>
 
-                {/* Page 2 */}
+                {/* Seite 2 */}
                 <div className='sticky top-[110px] h-screen bg-black '>
                     <div id="page2" className="w-[1272px] h-[792px]  bg-[#090909] z-20 absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
                         <div className='flex items-center w-full h-full '>
                             <div className='flex-row flex items-center space-x-[6rem]'>
                                 <img
                                     src="https://res.cloudinary.com/debiu7z1b/image/upload/v1730806886/Frame_45_cysssl.webp"
-                                    alt="Design & Development"
+                                    alt="Design & Entwicklung"
                                     className="h-[600px] w-[489px]"
                                 />
                                 <div className="flex flex-col space-y-2">
                                     <DesignIcon />
                                     <div className="flex flex-col">
-                                        <p className="text-white font-bold text-[36px]">Design & Development</p>
-                                        <p className="text-white text-xl">Building custom, user-friendly digital solutions.</p>
+                                        <p className="text-white font-bold text-[36px]">Design & Entwicklung</p>
+                                        <p className="text-white text-xl pr-10">Erstellung benutzerfreundlicher, maßgeschneiderter digitaler Lösungen.</p>
                                     </div>
                                 </div>
                             </div>
@@ -55,21 +55,21 @@ const Approach = () => {
                     </div>
                 </div>
 
-                {/* Page 3 */}
+                {/* Seite 3 */}
                 <div className='sticky top-[110px] h-screen bg-black '>
                     <div id="page3" className="w-[1272px] h-[792px]  bg-[#090909] z-30 absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
                         <div className='flex items-center w-full h-full '>
                             <div className='flex-row flex items-center space-x-[6rem]'>
                                 <img
                                     src="https://res.cloudinary.com/debiu7z1b/image/upload/v1730806887/Frame_46_llwwl9.webp"
-                                    alt="Launch & Optimization"
+                                    alt="Start & Optimierung"
                                     className="h-[600px] w-[489px]"
                                 />
                                 <div className="flex h-full flex-col space-y-2">
                                     <LaunchIcon />
                                     <div className="flex flex-col">
-                                        <p className="text-white font-bold text-[36px]">Launch & Optimization</p>
-                                        <p className="text-white text-xl">Seamless execution with ongoing support and optimization.</p>
+                                        <p className="text-white font-bold text-[36px]">Start & Optimierung</p>
+                                        <p className="text-white text-xl">Reibungslose Umsetzung mit fortlaufender Unterstützung und Optimierung.</p>
                                     </div>
                                 </div>
                             </div>
