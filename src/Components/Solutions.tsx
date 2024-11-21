0d344d3048ae53d1d29c05f6f29d070b087e957b
Feranmiola <88761161+Feranmiola@users.noreply.github.com>
2024-11-21 18:29:42 +0100

    "Updated Solutions component with responsive design changes for different screen sizes"

--- src/Components/Solutions.tsx
+++ src/Components/Solutions.tsx
@@ -2,49 +2,49 @@ import React from 'react'
 
 const Solutions = () => {
     return (
-        <div className='h-[799px] bg-white flex flex-col space-y-20 items-center justify-center w-full'>
-            <p className='text-[#080808] text-center w-[782px] text-[56px] leading-[4rem] font-semibold'>Digitale Lösungen, maßgeschneidert für eine <span className='text-deep-purple'>Vielzahl von Branchen.</span></p>
-            <div className='w-[1256px] h-[376px] flex flex-col items-center justify-between'>
-                <div className='w-full items-center justify-between flex flex-row'>
-                    <div className='flex flex-col px-5 py-5 space-y-3 w-[302px] h-[180px]'>
-                        <p className='text-2xl font-bold text-[#080808]'>Gesundheitswesen</p>
+        <div className='h-[799px] max-md:h-[1324px] bg-white flex flex-col space-y-20 max-md:space-y-10 items-center justify-center w-full'>
+            <p className='text-[#080808] text-center md:w-[782px] max-md:text-[24px] max-md:max-w-[90%] max-md:leading-[2.5rem] max-md:pt-10 max-md:text-left text-[56px] leading-[4rem] font-semibold'>Digitale Lösungen, maßgeschneidert für eine <span className='text-deep-purple'>Vielzahl von Branchen.</span></p>
+            <div className='w-[1256px] max-md:h-[1024px] max-md:w-[90%]  md:h-[376px] flex flex-col items-center md:justify-between'>
+                <div className='w-full items-center justify-between flex flex-row max-md:flex-col max-md:space-y-5 '>
+                    <div className='flex flex-col px-5 py-5 space-y-3 w-[302px] max-md:w-full h-[180px] max-md:h-[104px] max-md:space-y-2'>
+                        <p className='text-2xl font-bold max-md:text-xl text-[#080808]'>Gesundheitswesen</p>
                         <p className='text-base font-normal text-datkGrey'>Wir entwickeln sichere, HIPAA-konforme Plattformen, Patientenportale und Telemedizinlösungen.</p>
                     </div>
 
-                    <div className='flex flex-col px-5 py-5 space-y-3 w-[302px] h-[180px]'>
-                        <p className='text-2xl font-bold text-[#080808]'>E-Commerce</p>
+                    <div className='flex flex-col px-5 py-5 space-y-3 w-[302px] max-md:w-full h-[180px] max-md:h-[104px] max-md:space-y-2'>
+                        <p className='text-2xl font-bold max-md:text-xl text-[#080808]'>E-Commerce</p>
                         <p className='text-base font-normal text-datkGrey'>Individuelle Online-Shops mit benutzerfreundlichen Schnittstellen und nahtloser Zahlungsintegration.</p>
                     </div>
 
-                    <div className='flex flex-col px-5 py-5 space-y-3 w-[302px] h-[180px]'>
-                        <p className='text-2xl font-bold text-[#080808]'>Reise & Tourismus</p>
+                    <div className='flex flex-col px-5 py-5 space-y-3 w-[302px] max-md:w-full h-[180px] max-md:h-[104px] max-md:space-y-2'>
+                        <p className='text-2xl font-bold max-md:text-xl text-[#080808]'>Reise & Tourismus</p>
                         <p className='text-base font-normal text-datkGrey'>Individuelle Buchungsplattformen, Reise-Apps und Systeme zur Verwaltung von Reiserouten.</p>
                     </div>
 
-                    <div className='flex flex-col px-5 py-5 space-y-3 w-[302px] h-[180px]'>
-                        <p className='text-2xl font-bold text-[#080808]'>Finanzen</p>
+                    <div className='flex flex-col px-5 py-5 space-y-3 w-[302px] max-md:w-full h-[180px] max-md:h-[104px] max-md:space-y-2'>
+                        <p className='text-2xl font-bold max-md:text-xl text-[#080808]'>Finanzen</p>
                         <p className='text-base font-normal text-datkGrey'>Entwicklung robuster Fintech-Plattformen mit Sicherheits-, Compliance- und Analysefunktionen.</p>
                     </div>
                 </div>
 
-                <div className='w-full items-center justify-between flex flex-row'>
-                    <div className='flex flex-col px-5 py-5 space-y-3 w-[302px] h-[180px]'>
-                        <p className='text-2xl font-bold text-[#080808]'>Bildung</p>
+                <div className='w-full items-center justify-between flex flex-row max-md:flex-col max-md:space-y-5 '>
+                    <div className='flex flex-col px-5 py-5 space-y-3 w-[302px] max-md:w-full h-[180px] max-md:h-[104px] max-md:space-y-2'>
+                        <p className='text-2xl font-bold max-md:text-xl text-[#080808]'>Bildung</p>
                         <p className='text-base font-normal text-datkGrey'>Interaktive E-Learning-Plattformen mit Echtzeit-Kollaborationstools für Schüler und Lehrer.</p>
                     </div>
 
-                    <div className='flex flex-col px-5 py-5 space-y-3 w-[302px] h-[180px]'>
-                        <p className='text-2xl font-bold text-[#080808]'>Immobilien</p>
+                    <div className='flex flex-col px-5 py-5 space-y-3 w-[302px] max-md:w-full h-[180px] max-md:h-[104px] max-md:space-y-2'>
+                        <p className='text-2xl font-bold max-md:text-xl text-[#080808]'>Immobilien</p>
                         <p className='text-base font-normal text-datkGrey'>Digitale Lösungen für Immobilienangebote, virtuelle Touren und CRM-Systeme.</p>
                     </div>
 
-                    <div className='flex flex-col px-5 py-5 space-y-3 w-[302px] h-[180px]'>
-                        <p className='text-2xl font-bold text-[#080808]'>Logistik</p>
+                    <div className='flex flex-col px-5 py-5 space-y-3 w-[302px] max-md:w-full h-[180px] max-md:h-[104px] max-md:space-y-2'>
+                        <p className='text-2xl font-bold max-md:text-xl text-[#080808]'>Logistik</p>
                         <p className='text-base font-normal text-datkGrey'>End-to-End-Lösungen für Bestandsverwaltung, Sendungsverfolgung und Lieferkettenmanagement.</p>
                     </div>
 
-                    <div className='flex flex-col px-5 py-5 space-y-3 w-[302px] h-[180px]'>
-                        <p className='text-2xl font-bold text-[#080808]'>Einzelhandel</p>
+                    <div className='flex flex-col px-5 py-5 space-y-3 w-[302px] max-md:w-full h-[180px] max-md:h-[104px] max-md:space-y-2'>
+                        <p className='text-2xl font-bold max-md:text-xl text-[#080808]'>Einzelhandel</p>
                         <p className='text-base font-normal text-datkGrey'>Omnichannel-Einzelhandelsplattformen mit Bestandsverwaltung, POS-Integration und Tools für Kundenbindung.</p>
                     </div>
                 </div>
