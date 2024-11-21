976558c12c6b83c9b523bbfdcdcc525f3e6f55c6
Feranmiola <88761161+Feranmiola@users.noreply.github.com>
2024-11-21 19:15:27 +0100

    "Updated Footer component with new layout, added Webmacht icon, and rearranged elements"

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -7,6 +7,7 @@ import LinkIcon from './Icons/LinkIcon'
 import { useRouter } from 'next-nprogress-bar'
 import PhoneWhite from './Icons/PhoneWhite'
 import MailIcon from './Icons/MailIcon'
+import Webmacht from './Icons/Webmacht'
 
 const Footer = () => {
     const router = useRouter()
@@ -18,8 +19,55 @@ const Footer = () => {
     const [isPhoneHovered, setPhoneHovered] = useState(false)
 
     return (
-        <div className='bg-datkGrey flex w-full h-[331px] flex-col space-y-3'>
-            <div className='border-t-[1px] border-b-[1px] border-t-[#1B1B1B] flex items-center justify-center border-b-[#1B1B1B] w-full h-[294px]'>
+        <div className='bg-datkGrey flex w-full md:h-[331px] max-md:py-10 flex-col space-y-3'>
+            <div className='h-[480px] w-full flex flex-col items-center justify-center border-t-[1px] border-b-[1px] border-t-[#1B1B1B] border-b-[#1B1B1B]'>
+                <div className='w-[367px] h-[262px] flex flex-col space-y-5 px-5 justify-center border-l-[1px] border-r-[1px] border-l-[#1B1B1B] border-r-[#1B1B1B]'>
+                    <Webmacht />
+                    <div
+                        className='flex flex-row opacity-70 items-center space-x-2'>
+                        <LocationIcon />
+                        <p className='text-base text-white'>Zimmerstraße 78, 10117 Berlin, Deutschland</p>
+                    </div>
+
+                    <div
+                        className='flex flex-row opacity-70 items-center space-x-2'>
+                        <MailIcon />
+                        <p className='text-base text-white '>info@webmacht.de</p>
+                    </div>
+                    <div
+                        className='flex flex-row opacity-70 items-center space-x-2'>
+                        <PhoneWhite />
+                        <p className='text-base text-white '>+49 0 0900 090</p>
+                    </div>
+                </div>
+
+                <div className='w-full flex-1  flex items-center justify-center border-t-[1px] border-b-[1px] border-t-[#1B1B1B] border-b-[#1B1B1B]'>
+                    <div className='w-[367px] h-full items-center px-5 flex border-l-[1px] border-r-[1px] border-l-[#1B1B1B] border-r-[#1B1B1B]'>
+                        <p className='text-base text-white tracking-wider opacity-70'>SCHNELLZUGRIFF</p>
+                    </div>
+                </div>
+
+                <div className='w-[367px] flex px-5 justify-center  space-y-5 flex-col h-[168px] border-l-[1px] border-r-[1px] border-l-[#1B1B1B] border-r-[#1B1B1B]'>
+                    <div
+                        onClick={() => router.push('/impressum')}
+                        className='flex flex-row opacity-70 hover:opacity-100 cursor-pointer transition ease-in-out space-x-2 items-center'
+                    >
+                        <p className='text-base text-white '>Impressum</p>
+                        <LinkIcon />
+                    </div>
+                    <div className='flex flex-row space-x-2 opacity-70 hover:opacity-100 cursor-pointer transition ease-in-out items-center'>
+                        <p className='text-base text-white '>Datenschutzerklärung</p>
+                        <LinkIcon />
+                    </div>
+                    <div className='flex flex-row space-x-2 opacity-70 hover:opacity-100 cursor-pointer transition ease-in-out items-center'>
+                        <p className='text-base text-white '>Allgemeine Geschäftsbedingungen</p>
+                        <LinkIcon />
+                    </div>
+
+                </div>
+
+            </div>
+            <div className=' max-md:hidden border-t-[1px] border-b-[1px] border-t-[#1B1B1B] flex items-center justify-center border-b-[#1B1B1B] w-full h-[294px]'>
                 <div className='w-[1280px] h-full flex flex-row border-l-[1px] border-r-[#1B1B1B] border-r-[1px] border-l-[#1B1B1B]'>
 
                     {/* Webmacht-Symbol mit Skalierungs- und Schimmereffekt */}
