5d42e0db75013ffb2c6fb499638b1ae8de30099f
Feranmiola <88761161+Feranmiola@users.noreply.github.com>
2024-11-21 18:57:42 +0100

    "Updated styles and layout for Technologies component to improve responsiveness"

--- src/Components/Technologies.tsx
+++ src/Components/Technologies.tsx
@@ -11,13 +11,13 @@ import CrossPlatformIconWhite from './Icons/CrossPlatformIconWhite'
 const Technologies = () => {
     const [activeTabs, setActiveTabs] = useState(1)
     return (
-        <div className='h-[911px] w-full bg-[#F6FBFE] flex flex-col space-y-5 items-center justify-center'>
-            <div className='h-[137px] flex flex-col items-center justify-between'>
-                <p className='text-datkGrey text-[56px] font-semibold'>Technologien, die wir verwenden</p>
-                <p className='text-datkGrey text-base text-center w-[530px]'>Von maßgeschneidertem Webdesign bis hin zu Full-Stack-Entwicklung helfen wir Unternehmen, in der digitalen Welt zu wachsen.</p>
+        <div className='md:h-[911px] max-md:py-10 w-full bg-[#F6FBFE] flex flex-col space-y-5 max-md:space-y-10 items-center justify-center'>
+            <div className='h-[137px] flex flex-col items-center max-md:items-start px-5 justify-between'>
+                <p className='text-datkGrey text-[56px] max-md:text-2xl font-semibold'>Technologien, die wir verwenden</p>
+                <p className='text-datkGrey text-base text-center max-md:text-start md:w-[530px] max-sm:w-[90%]'>Von maßgeschneidertem Webdesign bis hin zu Full-Stack-Entwicklung helfen wir Unternehmen, in der digitalen Welt zu wachsen.</p>
             </div>
 
-            <div className='h-[54px] rounded-[500px] flex flex-row items-center justify-center space-x-3'>
+            <div className='md:h-[54px] rounded-[500px] flex flex-row max-md:flex-col max-md:space-y-1 md:items-center md:justify-center max-md:w-full max-md:items-start max-md:px-5 md:space-x-3'>
                 <div
                     onClick={() => setActiveTabs(1)}
                     className={`w-[216px] h-[46px] transition ease-in-out rounded-[200px] flex items-center justify-center ${activeTabs === 1 ? 'bg-deep-purple' : 'bg-transparent cursor-pointer hover:border-deep-purple'} border-[1px] border-transparent  flex-row space-x-1`}>
@@ -66,6 +66,14 @@ const Technologies = () => {
             <img
                 src='https://res.cloudinary.com/debiu7z1b/image/upload/v1730816529/Group_6021_e10y86.webp'
                 alt='Technologie-Icons'
+                className='max-md:hidden'
+            />
+
+
+            <img
+                src='https://res.cloudinary.com/debiu7z1b/image/upload/v1732211795/Frame_6020_evdedx.webp'
+                alt='Technologie-Icons'
+                className='md:hidden'
             />
 
         </div>
