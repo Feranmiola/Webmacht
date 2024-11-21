257e211932282226511e5f64d4b470aa9f5b4662
Feranmiola <88761161+Feranmiola@users.noreply.github.com>
2024-11-22 00:36:54 +0100

    "Added mobile-specific impressum content and updated desktop impressum layout"

--- src/app/impressum/page.tsx
+++ src/app/impressum/page.tsx
@@ -18,6 +18,52 @@ export default function Impressum() {
                     <ImpressumLinesMobile />
                 </div>
 
+                <div className="md:hidden flex flex-col  w-full max-w-[336px]">
+
+                    <div className="flex flex-row space-x-2 h-[91px] items-start border-b-[1px] border-b-[#0808081A]">
+                        <div>
+                            <LocationBlue />
+                        </div>
+                        <p className="text-base text-datkGrey">Webmacht <br /> Struthstr. 1, 63607 Wächtersbach, Deutschland</p>
+                    </div>
+
+
+                    <div className="flex flex-row space-x-2 h-[67px] items-center border-b-[1px] border-b-[#0808081A]">
+                        <div>
+                            <PhoneBlue />
+                        </div>
+                        <a href="tel:+4917564004044" className="text-base text-datkGrey">+49 175 640 4044</a>
+                    </div>
+
+                    <div className="flex flex-row space-x-2 h-[67px] items-center border-b-[1px] border-b-[#0808081A]">
+                        <div>
+                            <MailBlue />
+                        </div>
+                        <a href="mailto:info@webmacht.de" className="text-xl text-datkGrey">info@webmacht.de</a>
+                    </div>
+
+                    <div className="flex flex-col space-y-3 pt-4">
+                        <div className="flex flex-row items-center  space-x-2">
+                            <p className="text-datkGrey opacity-40 text-base">Geschäftsführer:</p>
+                            <p className="text-datkGrey text-base">Eyup Gözütok</p>
+                        </div>
+                        <div className="flex flex-row items-center space-x-2">
+                            <p className="text-datkGrey opacity-40 text-base">Amtsgericht:</p>
+                            <p className="text-datkGrey text-base">Port Yadira</p>
+                        </div>
+                        <div className="flex flex-row items-center space-x-2">
+                            <p className="text-datkGrey opacity-40 text-base">Handelsregisternummer:</p>
+                            <p className="text-datkGrey text-base">716-596-7996 x799</p>
+                        </div>
+                        <div className="flex flex-row items-center space-x-2">
+                            <p className="text-datkGrey opacity-40 text-base">USt-IdNr.:</p>
+                            <p className="text-datkGrey text-base">DE353989642</p>
+                        </div>
+
+                    </div>
+
+                </div>
+
                 <div className="w-[1234px] max-md:hidden border-[#0808081A] border-[1px] border-l-0 border-r-0 flex flex-row h-[220px] items-center">
                     <div className="h-full flex flex-col w-[694px] border-r-[1px] border-r-[#0808081A]">
                         <div className="flex flex-row space-x-2 h-[91px] items-center border-b-[1px] border-b-[#0808081A]">
@@ -31,14 +77,14 @@ export default function Impressum() {
                             <div>
                                 <PhoneBlue />
                             </div>
-                            <p className="text-xl text-datkGrey">+49 175 640 4044</p>
+                            <a href="tel:+4917564004044" className="text-xl text-datkGrey">+49 175 640 4044</a>
                         </div>
 
                         <div className="flex flex-row space-x-2 h-[62px] items-center ">
                             <div>
                                 <MailBlue />
                             </div>
-                            <p className="text-xl text-datkGrey">info@webmacht.de</p>
+                            <a href="mailto:info@webmacht.de" className="text-xl text-datkGrey">info@webmacht.de</a>
                         </div>
 
                     </div>
@@ -69,3 +115,4 @@ export default function Impressum() {
         </div>
     )
 }
+
