f3ff321ef4d8ff0234af143ef793054ebf24ab61
feranmiola <osunjuyigbeiyin@gmail.com>
2024-11-29 21:36:51 +0100

    Remove `//@ts-nocheck` directive and refactor `actionTypes` constant to an inline type definition.

--- src/hooks/use-toast.ts
+++ src/hooks/use-toast.ts
@@ -1,5 +1,4 @@
 "use client"
-//@ts-nocheck
 // Inspired by react-hot-toast library
 import * as React from "react"
 
@@ -18,12 +17,6 @@ type ToasterToast = ToastProps & {
   action?: ToastActionElement
 }
 
-const actionTypes = {
-  ADD_TOAST: "ADD_TOAST",
-  UPDATE_TOAST: "UPDATE_TOAST",
-  DISMISS_TOAST: "DISMISS_TOAST",
-  REMOVE_TOAST: "REMOVE_TOAST",
-} as const
 
 let count = 0
 
@@ -32,7 +25,12 @@ function genId() {
   return count.toString()
 }
 
-type ActionType = typeof actionTypes
+type ActionType = {
+  ADD_TOAST: "ADD_TOAST",
+  UPDATE_TOAST: "UPDATE_TOAST",
+  DISMISS_TOAST: "DISMISS_TOAST",
+  REMOVE_TOAST: "REMOVE_TOAST",
+}
 
 type Action =
   | {
